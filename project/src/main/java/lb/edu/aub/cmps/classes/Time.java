e6f2f89c44033fcacb953756f65896e7b63f5622
yasminkadah <fyk08@aub.edu.lb>
2015-11-05 01:36:03 +0200

    Implemented conflicts method in Time

--- project/src/main/java/lb/edu/aub/cmps/classes/Time.java
+++ project/src/main/java/lb/edu/aub/cmps/classes/Time.java
@@ -2,21 +2,49 @@ package lb.edu.aub.cmps.classes;
 
 import java.util.Arrays;
 
+import lb.edu.aub.cmps.enums.Day;
+
 public class Time {
 	private TimeSlot[] timeSlots;
-	
-	public Time(TimeSlot[] timeSlots){
+
+	public Time(TimeSlot[] timeSlots) {
 		this.timeSlots = timeSlots;
 	}
 
-	public String toString(){
+	public String toString() {
 		return Arrays.toString(timeSlots);
 	}
-	
-	public boolean conflicts(Time time2){
-		/**
-		 * TODO
-		 */
-		return false;
+
+	/**
+	 * This Method checks whether there is time conflict between this and time2
+	 * 
+	 * @param time2
+	 * @return boolean with true if conflict, false if no conflict
+	 */
+	public boolean conflicts(Time time2) {
+		boolean conflict = false;
+		for (int i = 0; i < this.timeSlots.length; i++) {
+			for (int j = 0; j < time2.timeSlots.length; j++) {
+				if (this.timeSlots[i].day == time2.timeSlots[j].day) {
+					if (Integer.parseInt(this.timeSlots[i].start) == Integer
+							.parseInt(time2.timeSlots[j].start)) {
+						conflict = true;
+					} else if (Integer.parseInt(this.timeSlots[i].start) > Integer
+							.parseInt(time2.timeSlots[j].start)
+							&& (Integer.parseInt(this.timeSlots[i].start) <= Integer
+									.parseInt(time2.timeSlots[j].end))) {
+						conflict = true;
+					} else if (Integer.parseInt(this.timeSlots[i].start) < Integer
+							.parseInt(time2.timeSlots[j].start)
+							&& (Integer.parseInt(this.timeSlots[i].end) >= Integer
+									.parseInt(time2.timeSlots[j].start))) {
+
+						conflict = true;
+					}
+				}
+			}
+
+		}
+		return conflict;
 	}
 }
