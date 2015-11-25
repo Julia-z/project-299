e53022bf2ff410474ebffa57d155c026a646276c
julia-z <juliaelzini@gmail.com>
2015-11-25 16:03:57 +0200

    main Packages cleanup. App.java class added

---
+++ project/src/main/java/lb/edu/aub/cmps/grad/App.java
@@ -0,0 +1,13 @@
+package lb.edu.aub.cmps.grad;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("main method in the App.java class....");
+	}
+
+}
