facbba265fa611fc51fc1534c81efbc3f357adf3
yasminkadah <fyk08@aub.edu.lb>
2015-12-01 01:41:19 +0200

    Updated PrintTimeClassInRoomVisitor,It now works!

--- project/src/main/java/lb/edu/aub/cmps/classes/PrintTimeClassInRoomVisitor.java
+++ project/src/main/java/lb/edu/aub/cmps/classes/PrintTimeClassInRoomVisitor.java
@@ -3,13 +3,21 @@ package lb.edu.aub.cmps.classes;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 
+import lb.edu.aub.cmps.algorithm.BasicScheduler;
+import lb.edu.aub.cmps.algorithm.ByTimeScheduler;
+import lb.edu.aub.cmps.algorithm.IScheduler;
+import lb.edu.aub.cmps.algorithm.SetUp;
 import lb.edu.aub.cmps.services.RoomService;
+import lb.edu.aub.cmps.classes.Day;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 
 public class PrintTimeClassInRoomVisitor implements RoomVisitor {
@@ -19,65 +27,127 @@ public class PrintTimeClassInRoomVisitor implements RoomVisitor {
 	HSSFWorkbook wb;
 	HSSFSheet Rooms;
 	int rowsCount;
-	
+	CellStyle gray;
+	CellStyle light_blue;
+	CellStyle red;
 	/**
 	 * Yasmin's fields
 	 */
 	private FileOutputStream output;
 	private String excelFileName;
-	
-	public PrintTimeClassInRoomVisitor(){
+
+	public PrintTimeClassInRoomVisitor() {
 		wb = new HSSFWorkbook();
 		Rooms = wb.createSheet("Rooms");
 		excelFileName = "Outputs\\Test.xls";
-		rowsCount = 0;
-
+		rowsCount = 1;
+		Rooms.setColumnWidth(0, 4000);
+		Rooms.setColumnWidth(1, 3000);
+		Rooms.setColumnWidth(2, 3000);
+		Rooms.setColumnWidth(3, 3000);
+		Rooms.setColumnWidth(4, 3000);
+		Rooms.setColumnWidth(5, 3000);
+		gray = wb.createCellStyle();
+		gray
+				.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+		gray.setFillPattern(CellStyle.SOLID_FOREGROUND);
+		light_blue = wb.createCellStyle();
+		light_blue.setFillForegroundColor(IndexedColors.LIGHT_CORNFLOWER_BLUE
+				.getIndex());
+		light_blue.setFillPattern(CellStyle.SOLID_FOREGROUND);
+		red = wb.createCellStyle();
+		red.setFillForegroundColor(IndexedColors.RED
+				.getIndex());
+		red.setFillPattern(CellStyle.SOLID_FOREGROUND);
+		Row firstRow = Rooms.createRow(0);
+		Cell roomName = firstRow.createCell(0);
+		roomName.setCellValue("Room Name");
+		Cell mon = firstRow.createCell(1);
+		mon.setCellValue("Monday");
+		Cell wed = firstRow.createCell(2);
+		wed.setCellValue("Wednesday");
+		Cell fri = firstRow.createCell(3);
+		fri.setCellValue("Friday");
+		Cell tues = firstRow.createCell(4);
+		tues.setCellValue("Tuesday");
+		Cell thurs = firstRow.createCell(5);
+		thurs.setCellValue("Thursday");
+		Cell sat = firstRow.createCell(6);
+		sat.setCellValue("Saturday");
+		mon.setCellStyle(gray);
+		wed.setCellStyle(gray);
+		fri.setCellStyle(gray);
+		tues.setCellStyle(gray);
+		thurs.setCellStyle(gray);
+		sat.setCellStyle(gray);
 	}
 
 	public void visit(Room r) {
-		Row row= Rooms.createRow(rowsCount);
+
+		Row row = Rooms.createRow(rowsCount);
 		rowsCount++;
-		Cell name= row.createCell(0);
-		name.setCellValue("jello");
+		Cell name = row.createCell(0);
 		name.setCellValue(r.getNumber());
-		for(TimeSlot t: r.getReserved().keySet() ){
-			Row row1= Rooms.createRow(rowsCount);
-			Cell name1= row1.createCell(1);
-			name1.setCellValue(t.toString());
-			
-			Cell course = row1.createCell(2);
-			//
-			course.setCellValue(r.getReserved().get(t).getCourse_id());
-			rowsCount ++;
+		name.setCellStyle(light_blue);
+		Row row1 = Rooms.createRow(rowsCount);
+
+		String Previous = "";
+		for (TimeSlot t : r.getReserved().keySet()) {
+			if (!t.toString().substring(3, 17).equals(Previous)) {
+				row1 = Rooms.createRow(rowsCount);
+				Cell firstCell = row1.createCell(0);
+				firstCell.setCellValue(t.toString().substring(3, 17));
+				if (r.getReserved().get(t).getGiven_room() != r.getReserved()
+						.get(t).getRequestedRoom()
+						|| r.getReserved().get(t).getGiven_time() != r.getReserved().get(t).getRequestedTime()) {
+					firstCell.setCellStyle(red);
+				}
+				Previous = t.toString().substring(3, 17);
+				rowsCount++;
+			}
+
+			if (t.getDay() == Day.M) {
+				Cell course = row1.createCell(1);
+				course.setCellValue(r.getReserved().get(t).getCourse_name());
+			} else if (t.getDay() == Day.W) {
+				Cell course = row1.createCell(2);
+				course.setCellValue(r.getReserved().get(t).getCourse_name());
+			} else if (t.getDay() == Day.F) {
+				Cell course = row1.createCell(3);
+				course.setCellValue(r.getReserved().get(t).getCourse_name());
+			} else if (t.getDay() == Day.T) {
+				Cell course = row1.createCell(4);
+				course.setCellValue(r.getReserved().get(t).getCourse_name());
+			} else if (t.getDay() == Day.R) {
+				Cell course = row1.createCell(5);
+				course.setCellValue(r.getReserved().get(t).getCourse_name());
+			}else if (t.getDay() == Day.S) {
+				Cell course = row1.createCell(6);
+				course.setCellValue(r.getReserved().get(t).getCourse_name());
+			}
+
 		}
-		
-		try{
-			output= new FileOutputStream(new File(excelFileName));
+
+		try {
+			output = new FileOutputStream(new File(excelFileName));
 			wb.write(output);
 			output.close();
-		}catch(IOException e){
+		} catch (IOException e) {
 		}
-		System.out.println("COUNTING gROWS"+rowsCount);
-		
-		
+
 	}
-	
-	public static void main(String[] args){
-		//rowsCount=0;
-	//	BasicScheduler sched = new BasicScheduler();
-		//sched.schedule();
-		//SetUp s = sched.setup();
-		System.out.println("_________________________________________________________________________________");
-		System.out.println("_________________________________________________________________________________");
-		System.out.println("_________________________________________________________________________________");
+
+	public static void main(String[] args) throws SecurityException,
+			IOException {
+		IScheduler s = new ByTimeScheduler();
+		s.schedule();
 		RoomVisitor visitor = new PrintTimeClassInRoomVisitor();
-		Set<Room> rooms = new RoomService().getAllRooms();
-		for(Room r: rooms){
-			r.initializeReserved();
-		}
-		for(Room r: rooms){
+		Map<Integer, Room> id_room = s.getIdRoomMap();
+		for (Integer i : id_room.keySet()) {
+			Room r = id_room.get(i);
 			r.accept(visitor);
 		}
+		
 	}
 
 }
