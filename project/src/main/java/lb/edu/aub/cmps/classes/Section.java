8544a4a0b077b5f98850abf49044530806f9e125
julia-z <juliaelzini@gmail.com>
2015-12-02 13:01:34 +0200

    Section Class done

--- project/src/main/java/lb/edu/aub/cmps/classes/Section.java
+++ project/src/main/java/lb/edu/aub/cmps/classes/Section.java
@@ -10,7 +10,7 @@ public class Section {
 	private int section_number;
 	private String course_name;
 	private int course_id;
-	private Set<Class> lectures;
+	private Set<Class> classes;
 	
 	public Section(int dep_id, String dep_name, int section_number,
 			String course_name, int course_id) {
@@ -20,7 +20,46 @@ public class Section {
 		this.section_number = section_number;
 		this.course_name = course_name;
 		this.course_id = course_id;
-		this.lectures = new HashSet<Class>();
+		this.classes = new HashSet<Class>();
+	}
+	public void addClass(Class c){
+		classes.add(c);
+	}
+	public int getDep_id() {
+		return dep_id;
+	}
+	public void setDep_id(int dep_id) {
+		this.dep_id = dep_id;
+	}
+	public String getDep_name() {
+		return dep_name;
+	}
+	public void setDep_name(String dep_name) {
+		this.dep_name = dep_name;
+	}
+	public int getSection_number() {
+		return section_number;
+	}
+	public void setSection_number(int section_number) {
+		this.section_number = section_number;
+	}
+	public String getCourse_name() {
+		return course_name;
+	}
+	public void setCourse_name(String course_name) {
+		this.course_name = course_name;
+	}
+	public int getCourse_id() {
+		return course_id;
+	}
+	public void setCourse_id(int course_id) {
+		this.course_id = course_id;
+	}
+	public Set<Class> getClasses() {
+		return classes;
+	}
+	public void setClasses(Set<Class> classes) {
+		this.classes = classes;
 	}
 
 }
